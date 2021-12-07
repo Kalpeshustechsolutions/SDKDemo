5b1ba90ad387a82e7887f28b539ca25d36400aba
DESKTOP-41339K6\Admin <Kalpesh.m@ustechsolutions.com>
2021-12-07 13:34:22 +0530

    Show Toast

--- sdklibrary/src/main/java/com/payu/sdklibrary/SplashActivity.kt
+++ sdklibrary/src/main/java/com/payu/sdklibrary/SplashActivity.kt
@@ -2,10 +2,17 @@ package com.payu.sdklibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        showToast()
+    }
+
+    fun showToast(){
+        Toast.makeText(this,"Welcome To Lib",Toast.LENGTH_SHORT).show()
     }
 }
